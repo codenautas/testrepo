d650a431fde9cbd8b2687f2b8a79df3037451aaa
Emilio Platzer <emilioplatzer@gmail.com>
2019-06-29 14:44:55 -0300

    Archivos de ejemplo

---
+++ test/test.js
@@ -0,0 +1,15 @@
+﻿"use strict";
+
+var _ = require('lodash');
+var expect = require('expect.js');
+var myNodeRepo = require('..');
+var fs = require('fs-promise');
+var expectCalled = require('expect-called');
+var Path = require('path');
+
+describe('mynoderepo', function(){
+    it('find git in myNodeRepo.config', function(done){
+        expect(1).to.eql(1);
+        done();
+    });
+});
